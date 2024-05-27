ad374ca6d7834f47205a8e0d0667532a32c267a8
Joao <jv.tortorello@gmail.com>
2024-05-26 22:51:02 -0300

    codigos final

--- Projeto-Final/critical-final.c
+++ Projeto-Final/critical-final.c
@@ -6,53 +6,49 @@
 // Função para calcular fatoriais e armazená-los em um vetor de precisão arbitrária
 void fatorial(int n, mpfr_t* vet, int nBits) {
     mpfr_init2(vet[0], nBits); // Primeiro elemento do vetor com precisão especificada
-    mpfr_set_d(vet[0], 1.0, MPFR_RNDU); // Primeiro elemento como 1.0
+    mpfr_set_ui(vet[0], 1.0, MPFR_RNDN); // Primeiro elemento como 1.0 (fatorial de 0)
 
-    mpfr_init2(vet[1], nBits); // Segundo elemento do vetor com precisão especificada
-    mpfr_set_d(vet[1], 1.0, MPFR_RNDU); // Segundo elemento como 1.0
-
-    // Loop para calcular fatoriais para índices maiores que 1
-    for (long int i = 2; i < n; ++i) {
+    // Loop para calcular fatoriais para índices maiores que 0
+    for (int i = 1; i <= n; i++) {
         mpfr_init2(vet[i], nBits); // Inicializa o elemento do vetor
-        mpfr_mul_si(vet[i], vet[i-1], i, MPFR_RNDU); // Calcula o fatorial e armazena no vetor
+        mpfr_mul_ui(vet[i], vet[i-1], i, MPFR_RNDN); // Calcula o fatorial e armazena no vetor
     }
-    return;
 }
 
 // Função para calcular a soma das inversas dos fatoriais
-void soma(int n, mpfr_t* vet, int nBits, mpfr_t* globalPointer) {
+void soma(int n, mpfr_t* vet, int nBits, mpfr_t partialSum) {
     int posicao = omp_get_thread_num(); // Obtém o número da thread atual
     int n_threads = omp_get_num_threads(); // Obtém o número total de threads
 
-    int parcela = n / n_threads; // Quantidade de elementos por thread
+    int parcela = n / n_threads; // Quantidade de elementos por thread que irá processar
     int inicio = parcela * posicao; // Índice de início para a thread atual
-    int fim = (posicao == n_threads - 1) ? n : inicio + parcela; // Índice de fim para a thread atual
+    int fim; // Índice de fim para a thread atual
+    if (posicao == n_threads - 1) { // Se for a última thread, processa até o fim do vetor
+        fim = n;
+    } else {
+        fim = inicio + parcela; // Caso contrário, processa até o índice calculado
+    }
 
-    mpfr_t parcial_local, divisao, um; // Variáveis temporárias para cálculos locais
+    mpfr_t parcial_local, divisao; // Variáveis temporárias para cálculos locais
 
     mpfr_init2(parcial_local, nBits); // Inicializa variável temporária para soma parcial local
-    mpfr_set_d(parcial_local, 0.0, MPFR_RNDU); // Define soma parcial local como 0.0
-
     mpfr_init2(divisao, nBits); // Inicializa variável temporária divisão
-    mpfr_set_d(divisao, 1.0, MPFR_RNDU); // Define divisão como 1.0
-
-    mpfr_init2(um, nBits); // Inicializa variável temporária um
-    mpfr_set_d(um, 1.0, MPFR_RNDU); // Define um como 1.0
+    mpfr_set_ui(divisao, 0, MPFR_RNDN); // Define a variável de divisão como 0
 
     // Loop para calcular a soma das inversas dos fatoriais
     for (int i = inicio; i < fim; i++) {
-        mpfr_div(divisao, um, vet[i], MPFR_RNDU); // Calcula a inversa do fatorial
-        mpfr_add(parcial_local, parcial_local, divisao, MPFR_RNDU); // Adiciona na soma parcial local
+        mpfr_ui_div(parcial_local, 1, vet[i], MPFR_RNDN); // Calcula a inversa do fatorial
+        mpfr_add(divisao, divisao, parcial_local, MPFR_RNDN); // Adiciona na soma parcial local
     }
 
-    #pragma omp critical // Início da região crítica para evitar condição de corrida
+    #pragma omp critical // Seção crítica para evitar condições de corrida
     {
-        mpfr_add(*globalPointer, *globalPointer, parcial_local, MPFR_RNDU); // Atualiza a soma global
+        mpfr_add(partialSum, partialSum, divisao, MPFR_RNDN);
     }
+
     // Libera a memória alocada
-    mpfr_clear(divisao); 
-    mpfr_clear(parcial_local);
-    mpfr_clear(um);
+    mpfr_clear(parcial_local); 
+    mpfr_clear(divisao);
 }
 
 int main(int argc, char* argv[]) {
@@ -67,22 +63,28 @@ int main(int argc, char* argv[]) {
 
     mpfr_t global; // Declaração da variável para armazenar a soma das inversas dos fatoriais
     mpfr_init2(global, nBits); // Inicializa a variável global com a precisão especificada
-    mpfr_set_d(global, 0.0, MPFR_RNDU); // Define a variável global como 0.0
+    mpfr_set_ui(global, 0.0, MPFR_RNDN); // Define a variável global como 0.0
 
     mpfr_t* vet = (mpfr_t*) malloc(n * sizeof(mpfr_t)); // Aloca memória para o vetor de fatoriais
-
     fatorial(n, vet, nBits); // Calcula os fatoriais e armazena no vetor
 
+    mpfr_t partialSum;
+    mpfr_init2(partialSum, nBits); // Inicializa partialSum
+
+    omp_set_num_threads(nThreads); // Define o número de threads a serem usadas
     #pragma omp parallel num_threads(nThreads)
     {
-        soma(n, vet, nBits, &global); // Chama a função para calcular a soma das inversas dos fatoriais
+        soma(n, vet, nBits, global); // Chama a função para calcular a soma das inversas dos fatoriais
     }
 
     printf("Resultado da aproximação de Euler: ");
-    mpfr_out_str(stdout, 10, 0, global, MPFR_RNDU);
+    mpfr_out_str(stdout, 10, 0, global, MPFR_RNDN);
     printf("\n");
 
-    mpfr_clear(global); // Libera a memória alocada para a variável global
+    // Libera a memória alocada
+    mpfr_clear(global);
+    mpfr_clear(partialSum);
+    free(vet);
 
     return 0;
 }
